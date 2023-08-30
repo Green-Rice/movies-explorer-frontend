1f38bdfa65ec7e83a1b2fc3a73d327e71483200b
Mao <104629274+Green-Rice@users.noreply.github.com>
2023-08-30 10:53:53 +0000

    Add header main navtab

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import NavTab from './NavTab/Navtab';
+import Promo from './Promo/Promo';
+
+const Main = () => {
+  return(
+    <main>
+      <Promo />
+      <NavTab />
+    </main>
+  );
+}
+
+export default Main;
