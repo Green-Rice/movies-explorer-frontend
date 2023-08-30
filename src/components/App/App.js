222c361f9e95f30f2159d35b987570187908badf
Mao <104629274+Green-Rice@users.noreply.github.com>
2023-08-30 11:58:50 +0000

    change app & header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route element={<Header loggedIn={loggedIn} />}>
+        <Route index element={<Main />} />
+      </Route>
+    </Routes>
   );
 }
 
