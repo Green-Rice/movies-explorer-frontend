6109f3bdddb66e877fd21a85ab68e6f15fc6c00d
Mao <104629274+Green-Rice@users.noreply.github.com>
2023-08-31 06:50:00 +0000

    add about, techs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
+import AboutProject from './AboutProject/AboutProject';
 import NavTab from './NavTab/Navtab';
 import Promo from './Promo/Promo';
+import Techs from './Techs/Techs';
 
 const Main = () => {
   return(
     <main>
       <Promo />
       <NavTab />
+      <AboutProject />
+      <Techs />
     </main>
   );
 }
