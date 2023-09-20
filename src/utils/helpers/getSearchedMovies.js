2e3ed086d39d8049bdbe3bfc9d22116b74157a28
Mao <104629274+Green-Rice@users.noreply.github.com>
2023-09-20 18:50:42 +0000

    Changes after 2st review

--- src/utils/helpers/getSearchedMovies.js
+++ src/utils/helpers/getSearchedMovies.js
@@ -1,6 +1,6 @@
 export const getSearchedMovies = (movies, movieName) =>
   movies.filter(
     (movie) =>
-      movie.nameRU.toLowerCase().includes(movieName) ||
-      movie.nameEN.toLowerCase().includes(movieName)
+      movie.nameRU.toLowerCase().includes(movieName.toLowerCase()) ||
+      movie.nameEN.toLowerCase().includes(movieName.toLowerCase())
   );
